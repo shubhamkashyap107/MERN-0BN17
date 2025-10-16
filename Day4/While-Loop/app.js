8aff5ae148477dd869569d9dd7eec0b9aa82b78e
Shubham Kashyap <shubhamkashyap107@gmail.com>
2025-10-16 13:29:00 +0530

    Commit

---
+++ Day4/While-Loop/app.js
@@ -0,0 +1,66 @@
+
+// let i = 1
+// while(i <= 999)
+// {
+//     console.log(i)
+//     i++
+// }
+
+// let i = 1
+// while(i <= 100)
+// {
+//     console.log("Shubham")
+//     // i = i + 1
+//     // i += 1
+//     i++
+// }
+
+
+
+// let i = 0
+// while(i < 3)
+// {
+//     console.log("Shubham")
+//     i++
+// }
+
+
+
+// let i = 5
+// while(i >= 1)
+// {
+//     console.log(i)
+//     i--
+// }
+
+
+// let i = 5
+// while(i > 0)
+// {
+//     console.log(i)
+//     i--
+// }
+
+
+
+
+
+
+// let a = Number(prompt("Please enter a"))
+// let b = Number(prompt("Please enter b"))
+// console.log(a + b)
+
+
+let i = 1
+let n = Number(prompt("Enter your number"))
+
+while(i <= n)
+{
+    console.log("Shubham")
+    // console.log(i)
+    i++
+}
+
+
+
+
---
+++ Day5/Methods/app.js
@@ -0,0 +1,114 @@
+// console.log(Math.round(12.3))
+// console.log(Math.round(12.8))
+
+
+
+// console.log(Math.floor(12.9))
+// console.log(Math.ceil(12.5))
+
+
+
+// let num = 12345
+
+// let ld = num % 10
+// console.log(ld)
+
+
+// num = Math.floor(num / 10)
+// console.log(num)
+
+
+// console.log(num % 10)
+// num = Math.floor(num / 10)
+// console.log(num)
+// console.log(num % 10)
+
+
+// let a = 10
+// let b = 22
+
+// console.log(Math.max(a,b))
+// console.log(Math.min(a,b))
+
+// let n = 3
+// let isPrime = true
+// let i = 2
+// while(i * i <= n)
+// {
+//     if(n % i == 0)
+//     {
+//         isPrime = false
+//         break
+//     }
+//     i++
+// }
+
+// if(isPrime == true)
+// {
+//     console.log(n + " is a prime number")
+// }
+// else
+// {
+//     console.log(n +" is not a prime number")
+// }
+
+
+
+
+
+let i = 1
+
+while(i <= 100)
+{
+    if(i == 6)
+    {
+        break
+    }
+    console.log(i)
+    i++
+}
+
+
+
+
+while(i <= 100)
+{
+    if(i == 3)
+    {
+        i++
+        continue
+    }
+    console.log(i)
+    i++
+}
+
+
+
+
+
+
+// while(i <= 100)
+// {
+
+//     if(i % 2 != 0)
+//     {
+//         i++
+//         continue
+//     }
+//     console.log(i)
+//     i++
+// }
+
+
+
+while(i <= 100)
+{
+
+    if(i % 5 != 0)
+    {
+        i++
+        continue
+    }
+    console.log(i)
+    i++
+}

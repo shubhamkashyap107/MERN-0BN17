4548a168190918372e27b867b880b8d466d31e97
Shubham <shubhamkashyap107@gmail.com>
2026-03-12 13:55:29 +0530

    Commit

---
+++ React/Day69/ReactSetup/src/App.jsx
@@ -0,0 +1,27 @@
+function App()
+{
+  console.log("Hello ji")
+  let age = 32
+
+
+  return (
+
+    <>
+    
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+    
+     {/* { KuchBHi()} */}
+     <KuchBHi />
+    </>
+  )
+
+}
+
+
+export function KuchBHi()
+{
+  return <h1>Hello from kb</h1>
+}
+export default App

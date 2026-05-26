bd8261361429421f4d86355bbe43e6813f59642b
Shubham <shubhamkashyap107@gmail.com>
2026-05-26 14:22:30 +0530

    Commit

--- Backend/Day6/mongoose/src/server.js
+++ Backend/Day6/mongoose/src/server.js
@@ -26,6 +26,12 @@ mongoose.connect(process.env.MONGO_URL)
 app.post("/todos", async(req, res) => {
     try {
         const{title, desc, isCompleted} = req.body
+
+        if(!title || !desc)
+        {
+            throw new Error("Please enter all the fields")
+        }
+
         const newTask = await Task.insertOne({title, desc, isCompleted})
         res.status(201).json(newTask)
         
@@ -37,7 +43,100 @@ app.post("/todos", async(req, res) => {
 })
 
 
+app.get("/todos", async(req, res) => {
+    try {
+        const allTasks = await Task.find()
+        res.status(200).json(allTasks)
+    } catch (error) {
+        res.status(400).json({
+            err : error.message
+        })
+    }
+})
+
+app.get("/todos/:id", async(req, res) => {
+    try {
+
+        const { id } = req.params
+        const foundTask = await Task.findById({_id : id})
+
+        if(!foundTask)
+        {
+            throw new Error("Task does not exist / incorrect ID")
+        }
+
+        res.status(200).json(foundTask)
 
 
+    } catch (error) {
+        res.status(400).json({
+            err : "Task does not exist / incorrect ID"
+        })        
+    }
+})
+
+app.put("/todos/:id", async(req, res) => {
+    try {
+        
+        const { id } = req.params
+        const{ title, desc } = req.body
+
+        if(!title || !desc)
+        {
+            throw new Error("Please enter all the fields")
+        }
+
+        // lamba kaam
+
+        const foundTask = await Task.findById(id)
+        if(!foundTask)
+        {
+            throw new Error("Incorrect ID / task not found")
+        }
+
+        foundTask.title = title
+        foundTask.desc = desc
+        await foundTask.save()
+        res.status(200).json(foundTask)
+
+
+        // find by id and update
+        // const updatedTask = await Task.findByIdAndUpdate(id, {title, desc}, {returnDocument  : "after", runValidators : true})
+        // res.status(200).json(updatedTask)
+
+    } catch (error) {
+        res.status(400).json({
+            err : "Incorrect ID / task not found",
+            details : error.message
+        })
+    }
+})
+
+app.patch("/todos/:id", async(req, res) => {
+    try {
+        const{id} = req.params
+        const{isCompleted} = req.body
+        const updatedTask = await Task.findByIdAndUpdate(id, {isCompleted}, {runValidators : true, returnDocument : "after"})
+        res.status(200).json(updatedTask)
+
+    } catch (error) {
+        res.status(400).json({
+            err : error.message
+        })
+    }
+})
+
+
+app.delete("/todos/:id", async(req, res) => {
+    try {
+        const{id} = req.params
+        const deletedTask = await Task.findByIdAndDelete(id)
+        res.status(200).json(deletedTask)
+    } catch (error) {
+        res.status(400).json({
+            err : error.message
+        })
+    }
+})
 
 
